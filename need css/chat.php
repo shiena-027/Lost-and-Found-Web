02edb30eb37b8c3e66ad3f7c9cf2328f6b970956
shiena-027 <mirandamaimai984@gmail.com>
2025-01-07 15:43:02 +0800

    Update chat.php

--- need css/chat.php
+++ need css/chat.php
@@ -65,7 +65,7 @@ if (isset($_GET['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="chat-style.css">
 </head>
 <body>
     <div class="chat-container">
