0be68d86cf4f2348a5259b113c2785ede45557b3
jocmpo <93925138+jocmpo@users.noreply.github.com>
2025-02-05 17:36:24 +0800

    Delete need css directory

--- need css/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear session variables
-session_destroy(); // Destroy the session
-header("Location: index.php"); // Redirect to login/home page
-exit();
-?>
